d17555e6313e0ff7a5d46bc00f16fab5cb01b10a
JimHuertas <live.land18@gmail.com>
2021-06-11 15:03:35 -0500

    read grammar, add to classes production n grammar

---
+++ grammar.h
@@ -0,0 +1,20 @@
+#ifndef GRAMMAR_H
+#define GRAMMAR_H
+#include "production.h"
+class Grammar{
+private:
+    vector<Production> productions;
+public:
+    Grammar(/* args */);
+    ~Grammar();
+};
+
+Grammar::Grammar(/* args */){
+}
+
+Grammar::~Grammar(){
+}
+
+
+
+#endif
